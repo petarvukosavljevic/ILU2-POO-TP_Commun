3bb6efef406ab9fffb22eb4711f1c0ecbbc55480
petarvukosavljevic <petar.vukosavljevic@univ-tlse3.fr>
2024-04-19 00:16:11 +0200

    resto

---
+++ src/model/Restaurant.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Restaurant {
+
+	public void ajouterTable(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int[] donnerPossibilites(FormulaireRestaurant formulaire) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ReservationRestaurant reserver(int i, FormulaireRestaurant formulaire) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
